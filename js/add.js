1fdbb7ff7eca45b2491d8b1d716a2da00e7957a5
Martin Goiriz <martingoiriz@gmail.com>
2025-11-28 13:32:38 -0300

    add verdu y panceta

--- js/add.js
+++ js/add.js
@@ -1,9 +1,9 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   const itemCounts = {};
 
   const total = {
     total: 0,
-    element: document.getElementById('total')
+    element: document.getElementById("total"),
   };
 
   function addItem(buttonId, counterId, totalCounter, itemName) {
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const counter = document.getElementById(counterId);
     let itemCount = 0;
 
-    button.addEventListener('click', function() {
+    button.addEventListener("click", function () {
       itemCount++;
       totalCounter.total++;
       itemCounts[itemName] = itemCount;
@@ -20,38 +20,41 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
-  addItem('clickAddCarne', 'countFrita', total, 'Carne Frita');
-  addItem('clickAddCarneCuchillo', 'countCarne', total, 'Carne Cuchillo');
-  addItem('clickAddPollo', 'countChicken', total, 'Pollo');
-  addItem('clickAddPolloCrema', 'countPolloCrema', total, 'Pollo a la Crema');
-  addItem('clickAddJyQ', 'countCheese', total, 'Jamon y Queso');
-  addItem('clickAddVerdura', 'countVeg', total, 'Verdura');
-  addItem('clickAddCaprese', 'countCaprese', total, 'Caprese');
-  addItem('clickAddChampi', 'countChampiñon', total, 'Champi');
-  addItem('clickAdd4Quesos', 'count4Quesos', total, '4 Quesos');
-  addItem('clickAddCalabresa', 'countCalabresa', total, 'Calabresa');
-  addItem('clickAddRoque', 'countRoquefort', total, 'Roquefort');
+  addItem("clickAddCarne", "countFrita", total, "Carne Frita");
+  addItem("clickAddCarneCuchillo", "countCarne", total, "Carne Cuchillo");
+  addItem("clickAddPollo", "countChicken", total, "Pollo");
+  addItem("clickAddPolloCrema", "countPolloCrema", total, "Pollo a la Crema");
+  addItem("clickAddJyQ", "countCheese", total, "Jamon y Queso");
+  addItem("clickAddVerdura", "countVeg", total, "Verdura");
+  addItem("clickAddCaprese", "countCaprese", total, "Caprese");
+  addItem("clickAddChampi", "countChampiñon", total, "Champi");
+  addItem("clickAdd4Quesos", "count4Quesos", total, "4 Quesos");
+  addItem("clickAddCalabresa", "countCalabresa", total, "Calabresa");
+  addItem("clickAddRoque", "countRoquefort", total, "Roquefort");
+  addItem("clickAddPanceta", "countPanceta", total, "Panceta y Ciruela");
+  addItem("clickAddVerdura", "countVerdura", total, "Verdura");
 
   function copyToClipboard() {
     const totalValue = total.element.textContent;
 
-    let copyText = 'Total: ' + totalValue + '\n' + '\n';
+    let copyText = "Total: " + totalValue + "\n" + "\n";
 
-    Object.keys(itemCounts).forEach(item => {
+    Object.keys(itemCounts).forEach((item) => {
       if (itemCounts[item] > 0) {
-        copyText += `${item}: ${itemCounts[item]}\n` + '\n';
+        copyText += `${item}: ${itemCounts[item]}\n` + "\n";
       }
     });
 
-    navigator.clipboard.writeText(copyText)
+    navigator.clipboard
+      .writeText(copyText)
       .then(() => {
-        console.log('Copied to clipboard:\n', copyText);
+        console.log("Copied to clipboard:\n", copyText);
       })
-      .catch(err => {
-        console.error('Failed to copy text: ', err);
+      .catch((err) => {
+        console.error("Failed to copy text: ", err);
       });
   }
 
-  const copyBtn = document.getElementById('copyBtn');
-  copyBtn.addEventListener('click', copyToClipboard);
+  const copyBtn = document.getElementById("copyBtn");
+  copyBtn.addEventListener("click", copyToClipboard);
 });
